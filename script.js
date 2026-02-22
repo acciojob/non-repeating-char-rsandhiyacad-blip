9c44a6bb45bd0030382f3df68aa49ae01e2a82b0
rsandhiyacad-blip <rsandhiyacad@gmail.com>
2026-02-22 13:01:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+let const={};
+for(let char of str){
+    count[char]=(count[char]||0)+1;
+}
+for(let char of str){
+    if(count[char]==1){
+        return char;
+    }
+}
+return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
